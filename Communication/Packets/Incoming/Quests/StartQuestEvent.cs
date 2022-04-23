a4bc09ed7fec548d18997126d050b10153708460
shinguweb96 <shinguweb@gmail.com>
2022-04-23 18:40:01 +0200

    Dependency Inject services into Quest Events

--- Communication/Packets/Incoming/Quests/StartQuestEvent.cs
+++ Communication/Packets/Incoming/Quests/StartQuestEvent.cs
@@ -1,23 +1,34 @@
 ﻿using Plus.Communication.Packets.Outgoing.Quests;
+using Plus.Database;
 using Plus.HabboHotel.GameClients;
+using Plus.HabboHotel.Quests;
 
 namespace Plus.Communication.Packets.Incoming.Quests;
 
 internal class StartQuestEvent : IPacketEvent
 {
+    private readonly IQuestManager _questManager;
+    private readonly IDatabase _database;
+
+    public StartQuestEvent(IQuestManager questManager, IDatabase database)
+    {
+        _questManager = questManager;
+        _database = database;
+    }
+
     public void Parse(GameClient session, ClientPacket packet)
     {
         var questId = packet.PopInt();
-        var quest = PlusEnvironment.GetGame().GetQuestManager().GetQuest(questId);
+        var quest = _questManager.GetQuest(questId);
         if (quest == null)
             return;
-        using (var dbClient = PlusEnvironment.GetDatabaseManager().GetQueryReactor())
+        using (var dbClient = _database.GetQueryReactor())
         {
             dbClient.RunQuery("REPLACE INTO `user_quests` (`user_id`,`quest_id`) VALUES ('" + session.GetHabbo().Id + "', '" + quest.Id + "')");
             dbClient.RunQuery("UPDATE `user_stats` SET `quest_id` = '" + quest.Id + "' WHERE `id` = '" + session.GetHabbo().Id + "' LIMIT 1");
         }
         session.GetHabbo().GetStats().QuestId = quest.Id;
-        PlusEnvironment.GetGame().GetQuestManager().GetList(session, null);
+        _questManager.GetList(session, null);
         session.SendPacket(new QuestStartedComposer(session, quest));
     }
 }
