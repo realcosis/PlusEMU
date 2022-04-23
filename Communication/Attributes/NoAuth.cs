d1b33dd12402709b50fb30774850092715c1f6cc
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-04-23 18:31:30 -0300

    NoAuth.cs attribute

---
+++ Communication/Attributes/NoAuth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Plus.Communication.Attributes;
+
+/// <summary>
+/// Indicate current packet the session doesn't need be logged-in.
+/// </summary>
+public class NoAuth : Attribute
+{
+    
+}
--- Communication/Packets/Incoming/Handshake/UniqueIDEvent.cs
+++ Communication/Packets/Incoming/Handshake/UniqueIDEvent.cs
@@ -1,8 +1,10 @@
-﻿using Plus.Communication.Packets.Outgoing.Handshake;
+﻿using Plus.Communication.Attributes;
+using Plus.Communication.Packets.Outgoing.Handshake;
 using Plus.HabboHotel.GameClients;
 
 namespace Plus.Communication.Packets.Incoming.Handshake;
 
+[NoAuth]
 public class UniqueIdEvent : IPacketEvent
 {
     public void Parse(GameClient session, ClientPacket packet)
