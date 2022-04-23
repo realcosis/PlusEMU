1c7cdc6e245965d8e05c97b28249244564efb49c
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-04-23 17:58:44 -0300

    fix ifigure data conflict

--- Core/FigureData/FigureDataManager.cs
+++ Core/FigureData/FigureDataManager.cs
@@ -6,26 +6,8 @@ using System.Xml;
 using NLog;
 using Plus.Core.FigureData.Types;
 using Plus.HabboHotel.Users.Clothing.Parts;
-using Plus.Utilities;
 
 namespace Plus.Core.FigureData;
-
-public interface IFigureDataManager
-{
-    void Init();
-    string ProcessFigure(string figure, string gender, ICollection<ClothingParts> clothingParts, bool hasHabboClub);
-    Palette GetPalette(int colorId);
-    bool TryGetPalette(int palletId, out Palette palette);
-    int GetRandomColor(int palletId);
-
-    private const string _defaultFigure =
-        "sh-3338-93.ea-1406-62.hr-831-49.ha-3331-92.hd-180-7.ch-3334-93-1408.lg-3337-92.ca-1813-62";
-
-    public static string FilterFigure(string figure)
-    {
-        return StringCharFilter.IsValid(figure) ? figure : _defaultFigure;
-    }
-}
 
 public class FigureDataManager : IFigureDataManager
 {
--- Core/FigureData/IFigureDataManager.cs
+++ Core/FigureData/IFigureDataManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Plus.Core.FigureData.Types;
 using Plus.HabboHotel.Users.Clothing.Parts;
+using Plus.Utilities;
 
 namespace Plus.Core.FigureData;
 
@@ -11,4 +12,11 @@ public interface IFigureDataManager
     Palette GetPalette(int colorId);
     bool TryGetPalette(int palletId, out Palette palette);
     int GetRandomColor(int palletId);
+    private const string DefaultFigure =
+        "sh-3338-93.ea-1406-62.hr-831-49.ha-3331-92.hd-180-7.ch-3334-93-1408.lg-3337-92.ca-1813-62";
+
+    public static string FilterFigure(string figure)
+    {
+        return StringCharFilter.IsValid(figure) ? figure : DefaultFigure;
+    }
 }
