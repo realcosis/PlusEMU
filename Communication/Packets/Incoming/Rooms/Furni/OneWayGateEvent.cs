5434dea1bfdbcf25cd92478ccbbd02ee6e21e6b4
Shetlands <Shetlands@DESKTOP-2910H9V>
2022-04-26 19:41:23 +0200

    adding the packet for the furniture OneWay Gate

---
+++ Communication/Packets/Incoming/Rooms/Furni/OneWayGateEvent.cs
@@ -0,0 +1,24 @@
+﻿using Plus.HabboHotel.GameClients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plus.Communication.Packets.Incoming.Rooms.Furni
+{
+    internal class OneWayGateEvent : IPacketEvent
+    {
+        public void Parse(GameClient session, ClientPacket packet)
+        {
+            var room = session.GetHabbo().CurrentRoom;
+            if (room == null)
+                return;
+            var item = room.GetRoomItemHandler().GetItem(packet.PopInt());
+            if (item == null)
+                return;
+            var hasRights = room.CheckRights(session);
+            item.Interactor.OnTrigger(session, item, -1, hasRights);
+        }
+    }
+}
