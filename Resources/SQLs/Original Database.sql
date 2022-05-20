c37cb04cfcd9c008adede36730767b963a317050
80O <48831268+80O@users.noreply.github.com>
2022-05-20 17:23:50 +0200

    Append SQL changes to original database file.

--- Resources/SQLs/Original Database.sql
+++ Resources/SQLs/Original Database.sql
@@ -25052,3 +25052,15 @@ ALTER TABLE `user_roomvisits`
 ALTER TABLE `rooms` 
 	ADD `sale_price` INT(5) NOT NULL DEFAULT '0' AFTER `spush_enabled`, 
 	ADD `lay_enabled` ENUM('0','1') NOT NULL DEFAULT '0' AFTER `sale_price`;
+
+-- 3_RefactorMessenger
+ALTER TABLE `messenger_friendships` DROP `id`;
+ALTER TABLE `messenger_friendships` ADD PRIMARY KEY(`user_one_id`, `user_two_id`);
+INSERT INTO `messenger_friendships` (`user_one_id`, `user_two_id`) SELECT `user_two_id`, `user_one_id` FROM `messenger_friendships`;
+ALTER TABLE `messenger_friendships` ADD `relationship` INT(1) NOT NULL DEFAULT '0' AFTER `user_two_id`;
+UPDATE `messenger_friendships` INNER JOIN `user_relationships` ON `messenger_friendships`.`user_one_id` = `user_relationships`.`user_id` AND `messenger_friendships`.`user_two_id` = `user_relationships`.`target` SET `messenger_friendships`.`relationship` = `type`;
+DROP TABLE ` user_relationships `;
+
+-- 4_IgnoredUsersTable
+ALTER TABLE `user_ignores` DROP `id`;
+ALTER TABLE `user_ignores` ADD PRIMARY KEY(`user_id`, `ignore_id`);
