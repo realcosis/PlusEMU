f654f569ffeff43feba75268c3b82085f4c0dc61
Shetlands <Shetlands@DESKTOP-2910H9V>
2022-05-21 15:48:38 +0200

    remove unnecessary usings

--- HabboHotel/Ambassadors/AmbassadorsManager.cs
+++ HabboHotel/Ambassadors/AmbassadorsManager.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Dapper;
 using Plus.Database;
-using Plus.HabboHotel.Users;
 using Plus.Utilities;
 
 namespace Plus.HabboHotel.Ambassadors
