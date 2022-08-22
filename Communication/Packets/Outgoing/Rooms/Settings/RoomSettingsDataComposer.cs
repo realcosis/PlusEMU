433eae9f74b427ad3479c36f5612bc8bc58fe5f6
matotubol <31701668+matotubol@users.noreply.github.com>
2022-08-22 17:58:41 +0200

    update

--- Communication/Packets/Outgoing/Rooms/Settings/RoomSettingsDataComposer.cs
+++ Communication/Packets/Outgoing/Rooms/Settings/RoomSettingsDataComposer.cs
@@ -26,10 +26,10 @@ public class RoomSettingsDataComposer : IServerPacket
         packet.WriteInteger(_room.Tags.Count);
         foreach (var tag in _room.Tags.ToArray()) packet.WriteString(tag);
         packet.WriteInteger(_room.TradeSettings); //Trade
-        packet.WriteInteger(_room.AllowPets); // allows pets in room - pet system lacking, so always off
-        packet.WriteInteger(_room.AllowPetsEating); // allows pets to eat your food - pet system lacking, so always off
-        packet.WriteInteger(_room.RoomBlockingEnabled);
-        packet.WriteInteger(_room.Hidewall);
+        packet.WriteInteger(_room.AllowPets ? 1 : 0); // allows pets in room - pet system lacking, so always off
+        packet.WriteInteger(_room.AllowPetsEating ? 1 : 0); // allows pets to eat your food - pet system lacking, so always off
+        packet.WriteInteger(_room.RoomBlockingEnabled ? 1 : 0);
+        packet.WriteInteger(_room.Hidewall ? 1 : 0);
         packet.WriteInteger(_room.WallThickness);
         packet.WriteInteger(_room.FloorThickness);
         packet.WriteInteger(_room.ChatMode); //Chat mode
