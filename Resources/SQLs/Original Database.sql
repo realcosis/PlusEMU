65b873fb9d12c425700d92622281fcaa9f31c6a3
matotubol <31701668+matotubol@users.noreply.github.com>
2022-08-22 11:49:50 +0200

    Forgot sql

--- Resources/SQLs/Original Database.sql
+++ Resources/SQLs/Original Database.sql
@@ -25085,6 +25085,7 @@ CREATE TABLE `ambassador_logs` (
 -- 8_AddBubbleIDToUsersTable
 ALTER TABLE `users` ADD COLUMN `bubble_id` TINYINT NOT NULL DEFAULT '0' AFTER `is_ambassador`;
 
+-- 9_ChangeRoomDataToBooleans
 ALTER TABLE `rooms` CHANGE `allow_pets` `allow_pets` BOOLEAN NOT NULL DEFAULT FALSE, 
 CHANGE `allow_pets_eat` `allow_pets_eat` BOOLEAN NOT NULL DEFAULT FALSE, 
 CHANGE `room_blocking_disabled` `room_blocking_disabled` BOOLEAN NOT NULL DEFAULT FALSE, 
