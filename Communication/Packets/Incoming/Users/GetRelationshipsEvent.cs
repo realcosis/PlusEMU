5b71a9bbdce0363d56052526b2367759c5dc2657
Josh <Diddyy@users.noreply.github.com>
2023-08-20 13:51:04 +0100

    Update GetRelationshipsEvent.cs

--- Communication/Packets/Incoming/Users/GetRelationshipsEvent.cs
+++ Communication/Packets/Incoming/Users/GetRelationshipsEvent.cs
@@ -1,16 +1,38 @@
-﻿using Plus.Communication.Packets.Outgoing.Users;
+using Plus.Communication.Packets.Outgoing.Users;
+using Plus.HabboHotel.Friends;
 using Plus.HabboHotel.GameClients;
+using Plus.HabboHotel.Users.Messenger;
 
 namespace Plus.Communication.Packets.Incoming.Users;
 
 internal class GetRelationshipsEvent : IPacketEvent
 {
-    public Task Parse(GameClient session, IIncomingPacket packet)
+    private readonly MessengerDataLoader _messengerDataLoader;
+    private readonly GameClientManager _gameClientManager;
+
+    public GetRelationshipsEvent(MessengerDataLoader messengerDataLoader, GameClientManager gameClientManager)
     {
-        var habbo = PlusEnvironment.GetHabboById(packet.ReadInt());
-        if (habbo == null)
-            return Task.CompletedTask;
-        session.Send(new GetRelationshipsComposer(habbo));
-        return Task.CompletedTask;
+        _messengerDataLoader = messengerDataLoader;
+        _gameClientManager = gameClientManager;
+    }
+
+    public async Task Parse(GameClient session, IIncomingPacket packet)
+    {
+        var userId = packet.ReadInt();
+        var client = _gameClientManager.GetClientByUserId(userId);
+        Dictionary<int, (MessengerBuddy buddy, int count)> relationships;
+
+        if (client != null)
+        {
+            // Online user logic
+            relationships = client.GetHabbo().Messenger.GetRelationships(); // Replace with actual method
+        }
+        else
+        {
+            // Offline user logic
+            relationships = await _messengerDataLoader.GetRelationshipsForUserAsync(userId);
+        }
+
+        session.Send(new GetRelationshipsComposer(userId, relationships));
     }
 }
