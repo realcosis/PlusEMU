2bec64f875fa57d037de27e7409166af75f197e2
Josh <Diddyy@users.noreply.github.com>
2023-08-20 14:18:28 +0100

    Update IHabboStatsService.cs

--- HabboHotel/Users/IHabboStatsService.cs
+++ HabboHotel/Users/IHabboStatsService.cs
@@ -1,6 +1,7 @@
-﻿namespace Plus.HabboHotel.Users;
+namespace Plus.HabboHotel.Users;
 
 public interface IHabboStatsService
 {
     Task<HabboStats> LoadHabboStats(int userId);
+    Task UpdateDailyRespectsAndTimestamp(int userId, int dailyRespects, string respectsTimestamp);
 }
