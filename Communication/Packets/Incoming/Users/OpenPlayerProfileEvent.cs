945d2651a13005672dc837f08234420d5f017e26
Josh <Diddyy@users.noreply.github.com>
2023-08-20 11:40:01 +0100

    Update OpenPlayerProfileEvent.cs

--- Communication/Packets/Incoming/Users/OpenPlayerProfileEvent.cs
+++ Communication/Packets/Incoming/Users/OpenPlayerProfileEvent.cs
@@ -1,7 +1,8 @@
-﻿using Plus.Communication.Packets.Outgoing.Users;
+using Plus.Communication.Packets.Outgoing.Users;
 using Plus.HabboHotel.Friends;
 using Plus.HabboHotel.GameClients;
 using Plus.HabboHotel.Groups;
+using Plus.HabboHotel.Users;
 using Plus.HabboHotel.Users.UserData;
 
 namespace Plus.Communication.Packets.Incoming.Users;
@@ -12,21 +13,24 @@ internal class OpenPlayerProfileEvent : IPacketEvent
     private readonly IMessengerDataLoader _messengerDataLoader;
     private readonly IGameClientManager _gameClientManager;
     private readonly IUserDataFactory _userDataFactory;
+    private readonly HabboStatsService _habboStatsService;
 
-    public OpenPlayerProfileEvent(IGroupManager groupManager, IMessengerDataLoader messengerDataLoader, IGameClientManager gameClientManager, IUserDataFactory userDataFactory)
+    public OpenPlayerProfileEvent(IGroupManager groupManager, IMessengerDataLoader messengerDataLoader, IGameClientManager gameClientManager, IUserDataFactory userDataFactory, HabboStatsService habboStatsService)
     {
         _groupManager = groupManager;
         _messengerDataLoader = messengerDataLoader;
         _gameClientManager = gameClientManager;
         _userDataFactory = userDataFactory;
+        _habboStatsService = habboStatsService;
     }
 
     public async Task Parse(GameClient session, IIncomingPacket packet)
     {
         var userId = packet.ReadInt();
         packet.ReadBool(); //IsMe?
-        
-        var targetData = _gameClientManager.GetClientByUserId(userId)?.GetHabbo()
+
+        var targetData = _gameClientManager.GetClientByUserId(userId)?.GetHabbo();
+
         targetData ??= await _userDataFactory.GetUserDataByIdAsync(userId);
 
         if (targetData == null)
@@ -34,9 +38,13 @@ internal class OpenPlayerProfileEvent : IPacketEvent
             session.SendNotification("An error occurred whilst finding that user's profile.");
             return;
         }
-        
+
+        // Load HabboStats here
+        var habboStats = await _habboStatsService.LoadHabboStats(userId);
+        targetData.HabboStats = habboStats;
+
         var groups = _groupManager.GetGroupsForUser(targetData.Id);
         var friendCount = await _messengerDataLoader.GetFriendCount(userId);
-        session.Send(new ProfileInformationComposer(targetData, session, groups, friendCount));
+        session.Send(new ProfileInformationComposer(targetData, session, groups, friendCount, habboStats));
     }
 }
