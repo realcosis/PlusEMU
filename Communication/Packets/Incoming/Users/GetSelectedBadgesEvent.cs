672d7b53b119b1886d2de3f54844e209b64a71d1
Josh <Diddyy@users.noreply.github.com>
2023-08-20 13:31:54 +0100

    Update GetSelectedBadgesEvent.cs

--- Communication/Packets/Incoming/Users/GetSelectedBadgesEvent.cs
+++ Communication/Packets/Incoming/Users/GetSelectedBadgesEvent.cs
@@ -1,17 +1,37 @@
-﻿using Plus.Communication.Packets.Outgoing.Users;
+using Plus.Communication.Packets.Outgoing.Users;
 using Plus.HabboHotel.GameClients;
+using Plus.HabboHotel.Users.Badges;
+using Plus.HabboHotel.Users.UserData;
 
 namespace Plus.Communication.Packets.Incoming.Users;
 
 internal class GetSelectedBadgesEvent : IPacketEvent
 {
-    public Task Parse(GameClient session, IIncomingPacket packet)
+    private readonly UserDataFactory _userDataFactory;
+    private readonly GameClientManager _gameClientManager;
+
+    public GetSelectedBadgesEvent(UserDataFactory userDataFactory, GameClientManager gameClientManager)
+    {
+        _userDataFactory = userDataFactory;
+        _gameClientManager = gameClientManager;
+    }
+
+    public async Task Parse(GameClient session, IIncomingPacket packet)
     {
         var userId = packet.ReadInt();
-        var habbo = PlusEnvironment.GetHabboById(userId);
-        if (habbo == null)
-            return Task.CompletedTask;
-        session.Send(new HabboUserBadgesComposer(habbo));
-        return Task.CompletedTask;
+        var gameClient = _gameClientManager.GetClientByUserId(userId);
+        var habbo = gameClient?.GetHabbo();
+        List<Badge> equippedBadges;
+
+        if (habbo != null && habbo.Inventory?.Badges != null)
+        {
+            equippedBadges = habbo.Inventory.Badges.EquippedBadges;
+        }
+        else
+        {
+            equippedBadges = await _userDataFactory.GetEquippedBadgesForUserAsync(userId);
+        }
+
+        session.Send(new HabboUserBadgesComposer(userId, equippedBadges));
     }
 }
